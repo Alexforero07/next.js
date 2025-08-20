f0eade50db453a6cc2aa7c7a77121f57ea9a0b3d
Alexforero07 <alexdjstiven@gmail.com>
2025-08-19 23:54:15 -0500

    segundo commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@
 export default function Home() {
   // Ejercicio 1 – Lista de productos con categorias
   const productos = [
-    { id: 1, nombre: "Camiseta", precio: 25, categoria: { id: 1, nombre: "Ropa" } },
+    { id: 1, nombre: "Camisetaa", precio: 25, categoria: { id: 1, nombre: "Ropa" } },
     { id: 2, nombre: "Zapatos deportivos", precio: 60, categoria: { id: 2, nombre: "Calzado" } },
     { id: 3, nombre: "Gorra", precio: 15, categoria: { id: 1, nombre: "Ropa" } },
     { id: 4, nombre: "Pantalón", precio: 40, categoria: { id: 1, nombre: "Ropa" } },
